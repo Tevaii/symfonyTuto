131805cd12fc7fa2bb548264b3437f9ba2ba2ec7
Tevai <tevaiheiariinuiitepaotahi@hotmail.fr>
2018-08-23 13:50:13 +0200

    no message

---
+++ README.md
@@ -0,0 +1,4 @@
+Symfony
+=======
+
+A Symfony project created on August 22, 2018, 12:21 pm.
---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,51 @@
+<?php
+
+// src/OC/PlatformBundle/Controller/AdvertController.php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AdvertController extends Controller
+{
+
+    //La route qui fait appel çà cette fonction est OCPlatorfmBundle:Adver:view
+    //$id correspond à l'argument $id présent dans l'url (trucs/{id})
+    //on injecte la requete dans les arguments de la fonction
+    public function viewAction($id,Request $request)
+    {/*
+        $tag = $request->query->get('tag');
+
+        return new Response(
+            "Affichage de l'annonce d'id : ".$id.", avec le tag : ".$tag
+        );
+*/
+    }
+
+    //Creation d'une vue plus complexe avec un slug
+
+    public function viewSlugAction($slug,$year,$format)
+    {
+        return new Response(
+            "Affichage de l'année '".$year."
+            ' du format '".$format."' et du slug '".$slug."'.
+            "
+        );
+    }
+
+
+    //fonction de génération d'une url
+    public function indexAction()
+    {
+        //génération d'une url pour l'annonce 5
+        //1er argument : nom de la route
+        //2eme argument valeurs des paramètres
+        $url = $this->get('router')->generate('oc_platform_view',array('id'=>5));
+
+        //url vaut : "/platform/advert/5"
+        return new Response("L'URL de l'annonce d'if 5 est : ".$url);
+    }
+}
+?>
