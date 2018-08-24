f14072664da617639716f6f1423edabdc2c4c7f8
tevai <tevaiheiariinuiitepaotahi@hotmail.fr>
2018-08-24 14:45:49 +0200

    Ajout de parametres au service antispam

--- src/OC/PlatformBundle/Antispam/OCAntispam.php
+++ src/OC/PlatformBundle/Antispam/OCAntispam.php
@@ -5,6 +5,17 @@ namespace OC\PlatformBundle\Antispam;
 
 class OCAntispam
 {
+    private $mailer;
+    private $locale;
+    private $minLength;
+
+    public function __construct(\Swift_Mailer $mailer, $locale, $minLength)
+    {
+        $this->mailer    = $mailer;
+        $this->locale    = $locale;
+        $this->minLength = (int) $minLength;
+    }
+
     /**
      * Vérifie si le texte est un spam ou non
      *
@@ -13,6 +24,7 @@ class OCAntispam
      */
     public function isSpam($text)
     {
-        return strlen($text) < 50;
+        return strlen($text) < $this->minLength;
     }
+
 }
