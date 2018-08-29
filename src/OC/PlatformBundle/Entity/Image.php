51d1b2a54a6216f9044d55c7de9e72bd965cb47b
tevai <tevaiheiariinuiitepaotahi@hotmail.fr>
2018-08-29 16:23:36 +0200

    clear project

--- src/OC/PlatformBundle/Entity/Image.php
+++ src/OC/PlatformBundle/Entity/Image.php
@@ -1,5 +1,5 @@
 <?php
-// src/OC/PlatformBundle/Entity/Image.php
+// src/OC/PlatformBundle/Entity/Image
 
 namespace OC\PlatformBundle\Entity;
 
@@ -11,22 +11,60 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Image
 {
-    /**
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+  /**
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  private $id;
 
-    /**
-     * @ORM\Column(name="url", type="string", length=255)
-     */
-    private $url;
+  /**
+   * @ORM\Column(name="url", type="string", length=255)
+   */
+  private $url;
 
-    /**
-     * @ORM\Column(name="alt", type="string", length=255)
-     */
-    private $alt;
+  /**
+   * @ORM\Column(name="alt", type="string", length=255)
+   */
+  private $alt;
 
-    // Getters et setters
+  /**
+   * @return int
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  /**
+   * @param string $url
+   */
+  public function setUrl($url)
+  {
+    $this->url = $url;
+  }
+
+  /**
+   * @return string
+   */
+  public function getUrl()
+  {
+    return $this->url;
+  }
+
+  /**
+   * @param string $alt
+   */
+  public function setAlt($alt)
+  {
+    $this->alt = $alt;
+  }
+
+  /**
+   * @return string
+   */
+  public function getAlt()
+  {
+    return $this->alt;
+  }
 }
--- src/OC/PlatformBundle/Entity/Skill.php
+++ src/OC/PlatformBundle/Entity/Skill.php
@@ -6,21 +6,44 @@ namespace OC\PlatformBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="OC\PlatformBundle\Entity\SkillRepository")
+ * @ORM\Entity
+ * @ORM\Table(name="oc_skill")
  */
 class Skill
 {
-    /**
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+  /**
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  private $id;
 
-    /**
-     * @ORM\Column(name="name", type="string", length=255)
-     */
-    private $name;
+  /**
+   * @ORM\Column(name="name", type="string", length=255)
+   */
+  private $name;
 
-    // Getters et setters
+  /**
+   * @return integer
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  /**
+   * @param string $name
+   */
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+
+  /**
+   * @return string
+   */
+  public function getName()
+  {
+    return $this->name;
+  }
 }
