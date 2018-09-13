a3c41047dd1e6b1584f0b5ee62da3682761ec3b1
tevai <tevaiheiariinuiitepaotahi@hotmail.fr>
2018-09-13 17:10:26 +0200

    ajout extension atispam twig

---
+++ src/OC/PlatformBundle/Twig/AntispamExtension.php
@@ -0,0 +1,34 @@
+<?php
+// src/OC/PlatformBundle/Twig/AntispamExtension.php
+
+namespace OC\PlatformBundle\Twig;
+
+use OC\PlatformBundle\Antispam\OCAntispam;
+
+class AntispamExtension extends \Twig_Extension
+{
+    /**
+     * @var OCAntispam
+     */
+    private $ocAntispam;
+
+    public function __construct(OCAntispam $ocAntispam)
+    {
+        $this->ocAntispam = $ocAntispam;
+    }
+
+    public function checkIfArgumentIsSpam($text)
+    {
+        return $this->ocAntispam->isSpam($text);
+    }
+
+    public function getFunctions()
+    {
+        return array(new \Twig_SimpleFunction('checkIfSpam',array($this,'checkIfArgumentsIsSpam')));
+    }
+
+    public function getName()
+    {
+        return 'OCAntispam';
+    }
+}
